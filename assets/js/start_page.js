e38cf116184c5dab18306dfb9e362b92d01a4c7e
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-05 21:45:58 -0500

    Added the first start page and empty high_scores.html

---
+++ assets/js/start_page.js
@@ -0,0 +1,58 @@
+// Code Quiz Start Page
+console.log("Let's Start the Code Quiz")
+
+// add timer starting at 0
+let time = document.createElement("span");
+time.textContent = 0;
+console.log(time)
+
+
+let time_display = document.createElement("div");
+time_display.textContent = "Time: " ;
+console.log(time_display)
+
+//add heading
+let h1_code_quiz = document.createElement("h1");
+h1_code_quiz.textContent = "Code Quiz";
+h1_code_quiz.setAttribute("style", "text-align: center");
+console.log(h1_code_quiz);
+
+
+// Create form
+let start_form = document.createElement("form");
+// add quiz question types
+let quiz_types = document.createElement("select")
+quiz_types.setAttribute("name", "quiz_types");
+quiz_types.setAttribute("multiple", true)
+let quiz_type_options = ["html", "css", "javascript", "jquery"]
+for (let index = 0; index < quiz_type_options.length; index++) {
+    let quiz_type_option = document.createElement("option");
+    quiz_type_option.setAttribute("id", index);
+    quiz_type_option.setAttribute("value", quiz_type_options[index]);
+    quiz_type_option.textContent = quiz_type_options[index];
+
+    console.log(quiz_type_option)
+    quiz_types.appendChild(quiz_type_option);
+
+}
+console.log(quiz_types);
+// add Start Quiz Button
+let start_quiz_btn = document.createElement("button");
+start_quiz_btn.textContent = "Start Quiz";
+
+console.log(start_form);
+
+// add view high scores link
+let view_high_scores_link = document.createElement("a");
+view_high_scores_link.textContent = "View High Scores"
+view_high_scores_link.setAttribute("href", "./high_scores.html")
+console.log(view_high_scores_link)
+
+//Add elements to page
+document.body.appendChild(time_display)
+time_display.appendChild(time);
+document.body.appendChild(h1_code_quiz);
+start_form.appendChild(quiz_types);
+start_form.appendChild(start_quiz_btn)
+document.body.appendChild(start_form)
+document.body.appendChild(view_high_scores_link)
