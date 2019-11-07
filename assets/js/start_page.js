11ffd9f89a597fac47b4960432159b9edc41c1bf
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-07 11:18:44 -0500

    Added divs to separate elements on the page; Applied js best practices from W3 schools https://www.w3schools.com/js/js_best_practices.asp

--- assets/js/start_page.js
+++ assets/js/start_page.js
@@ -1,30 +1,51 @@
+let time_div, time_display, time;
+let code_quiz_div, code_quiz_h1;
+let start_form_div, start_form;
+let quiz_type_div, quiz_types, quiz_type_options, quiz_type_option;
+let start_button_div, start_quiz_btn;
+let high_scores_div, view_high_scores_link;
+
 // Code Quiz Start Page
 console.log("Let's Start the Code Quiz")
 
+// create page areas for time, code quiz header, start quiz form, and high scores.
+time_div = document.createElement("div");
+code_quiz_div = document.createElement("div");
+start_form_div = document.createElement("div");
+quiz_type_div = document.createElement("div");
+start_button_div = document.createElement("div");
+high_scores_div = document.createElement("div");
+
+time_div.setAttribute("id", "time-area");
+code_quiz_div.setAttribute("id", "code_quiz_heading_area");
+start_form_div.setAttribute("id", "form_area");
+quiz_type_div.setAttribute("id", "quiz_type_area");
+start_button_div.setAttribute("id", "start_button_area");
+
 // add timer starting at 0
-let time = document.createElement("span");
+time = document.createElement("span");
 time.textContent = 0;
 console.log(time)
 
 
-let time_display = document.createElement("div");
+time_display = document.createElement("p");
 time_display.textContent = "Time: " ;
 console.log(time_display)
 
 //add heading
-let h1_code_quiz = document.createElement("h1");
+h1_code_quiz = document.createElement("h1");
 h1_code_quiz.textContent = "Code Quiz";
 h1_code_quiz.setAttribute("style", "text-align: center");
 console.log(h1_code_quiz);
 
 
 // Create form
-let start_form = document.createElement("form");
+start_form = document.createElement("form");
 // add quiz question types
-let quiz_types = document.createElement("select")
+quiz_types = document.createElement("select")
 quiz_types.setAttribute("name", "quiz_types");
 quiz_types.setAttribute("multiple", true)
-let quiz_type_options = ["html", "css", "javascript", "jquery"]
+quiz_type_options = ["html", "css", "javascript", "jquery"]
 for (let index = 0; index < quiz_type_options.length; index++) {
     let quiz_type_option = document.createElement("option");
     quiz_type_option.setAttribute("id", index);
@@ -41,7 +62,7 @@ for (let index = 0; index < quiz_type_options.length; index++) {
 
 console.log(quiz_types);
 // add Start Quiz Button
-let start_quiz_btn = document.createElement("button");
+start_quiz_btn = document.createElement("button");
 start_quiz_btn.setAttribute("id", "start_quiz");
 start_quiz_btn.setAttribute("class", "start_quiz");
 start_quiz_btn.textContent = "Start Quiz";
@@ -56,7 +77,7 @@ console.log(start_quiz_btn)
 console.log(start_form);
 
 // add view high scores link
-let view_high_scores_link = document.createElement("a");
+view_high_scores_link = document.createElement("a");
 view_high_scores_link.textContent = "View High Scores"
 view_high_scores_link.setAttribute("href", "./high_scores.html")
 console.log(view_high_scores_link)
@@ -64,16 +85,22 @@ console.log(view_high_scores_link)
 //Add elements to page
 
 // - append timer elements
-document.body.appendChild(time_display)
 time_display.appendChild(time);
+time_div.appendChild(time_display);
+document.body.appendChild(time_div);
 
 // - append heading
-document.body.appendChild(h1_code_quiz);
+code_quiz_div.appendChild(h1_code_quiz);
+document.body.appendChild(code_quiz_div);
 
 // - append form elements: quiz types and start button
-start_form.appendChild(quiz_types);
-start_form.appendChild(start_quiz_btn)
-document.body.appendChild(start_form)
+quiz_type_div.appendChild(quiz_types)
+start_form.appendChild(quiz_type_div);
+start_button_div.appendChild(start_quiz_btn);
+start_form.appendChild(start_button_div);
+start_form_div.appendChild(start_form);
+document.body.appendChild(start_form_div);
 
 // - append high scores link
-document.body.appendChild(view_high_scores_link)
+high_scores_div.appendChild(view_high_scores_link);
+document.body.appendChild(high_scores_div);
