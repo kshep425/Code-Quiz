ee465b2fbe8c3c4f8ec2c2b1a42c59bd5e6f3435
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-08 03:06:43 -0500

    Reformatted, reordered, added bootstrap classes

--- assets/js/complete_page.js
+++ assets/js/complete_page.js
@@ -3,22 +3,38 @@
     - save their high score with their name
 */
 
+// Declare all variables
+let container_div,
+    time_comp_div, time_comp_p, time_comp_sp, time_comp_left,   // time area
+    code_quiz_div, code_quiz_h1,                                // code quiz header area
+    great_job_div, great_job_h1,                                // great job header area
+    form_div, form_form,                                        // form area
+    score_h2,
+    player_name_div, player_name,
+    high_score_button
+
+// Container div
+container_div = document.createElement("div");
+container_div.setAttribute("class", "container text-center")
+document.body.appendChild(container_div)
+
 // Time
 //  - create time area
-let time_comp_div = document.createElement("div");
+time_comp_div = document.createElement("div");
 time_comp_div.setAttribute("id", "time_area");
+time_comp_div.setAttribute("class", "text-right my-3")
 
 //  - Create text content Time: <time_left>
-let time_comp_p = document.createElement("p");
-let time_comp_sp = document.createElement("span");
-let time_comp_left = localStorage.getItem("score");
+time_comp_p = document.createElement("p");
+time_comp_sp = document.createElement("span");
+time_comp_left = localStorage.getItem("score");
 time_comp_p.textContent = "Time: ";
 time_comp_sp.textContent = time_comp_left;
 
 // - Add text content to time_div
 time_comp_p.appendChild(time_comp_sp);
 time_comp_div.appendChild(time_comp_p);
-document.body.appendChild(time_comp_div);
+container_div.appendChild(time_comp_div);
 
 // Great Job Header
 // - Create header div
@@ -27,51 +43,66 @@ code_quiz_div.setAttribute("id", "code_quiz_div");
 code_quiz_h1 = document.createElement("h1");
 code_quiz_h1.textContent = "Code Quiz";
 code_quiz_h1.setAttribute("id", "code_quiz_h1");
+code_quiz_div.setAttribute("class", "my-3")
 code_quiz_div.appendChild(code_quiz_h1);
-document.body.appendChild(code_quiz_div);
+container_div.appendChild(code_quiz_div);
 
 // Great Job Header
 // - Create header div
-let great_job_div = document.createElement("div")
+great_job_div = document.createElement("div")
 great_job_div.setAttribute("id", "great_job_div");
 
-let great_job_h1 = document.createElement("h1");
+great_job_h1 = document.createElement("h1");
 great_job_h1.textContent = "Great Job!";
 great_job_h1.setAttribute("id", "great_job_h1");
+great_job_h1.setAttribute("class", "my-3")
 
 great_job_div.appendChild(great_job_h1);
-document.body.appendChild(great_job_div);
+container_div.appendChild(great_job_div);
 
 
 // Form area
-let form_div = document.createElement("div");
-let form_form = document.createElement("form");
+form_div = document.createElement("div");
+form_form = document.createElement("form");
+form_form.setAttribute("class", "my-3")
 form_form.setAttribute("id", "high_score_form");
 
 // - Display Score
-let score_h2 = document.createElement("h2");
+score_h2 = document.createElement("h2");
+score_h2.setAttribute("class", "my-3")
 score_h2.textContent = "Your score is: " + time_comp_left;
 
 // - Input Name
-let player_name_div = document.createElement("div");
-player_name_div.textContent = "Enter Name: ";
+player_name_div = document.createElement("div");
+player_name_div.setAttribute("class", "input-group my-3");
 
-let player_name = document.createElement("input");
+player_name_div_2 = document.createElement("div");
+player_name_div_2.setAttribute("class", "input-group-prepend mx-auto")
+
+player_name_text = document.createElement("span")
+player_name_text.setAttribute("class", "input-group-text")
+player_name_text.textContent = "Enter Name: ";
+
+player_name = document.createElement("input");
 player_name.setAttribute("type", "text");
-player_name.setAttribute("name", "player_name")
+player_name.setAttribute("name", "player_name");
+player_name.setAttribute("class", "form-control")
 
 // - Submit High Score Button
-let high_score_button = document.createElement("button");
+high_score_button = document.createElement("button");
 high_score_button.textContent = "Submit High Score";
+high_score_button.setAttribute("class", "btn btn-primary")
 
 
 // Append to window
 form_form.appendChild(score_h2);
-player_name_div.appendChild(player_name);
+player_name_text.appendChild(player_name)
+player_name_div_2.appendChild(player_name_text)
+player_name_div.appendChild(player_name_div_2)
 form_form.appendChild(player_name_div);
 form_form.appendChild(high_score_button)
 form_div.appendChild(form_form);
-document.body.appendChild(form_div);
+container_div.appendChild(form_div);
 
 
 // Click Event Listener for 'Submit High Score' Button
